15a9c1950d93b525d56ea674bbfba5182a88b0e9
FormX <hello@formx.ai>
2023-06-14 08:36:27 +0000

    release: v0.1.0

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FormX",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "FormX",
+            targets: ["FormX"])
+    ],
+    dependencies: [
+        .package(name: "opencv2",
+                 url: "https://github.com/oursky/opencv.git",
+                 .branch("4.6.0"))
+    ],
+    targets: [
+        .binaryTarget(name: "opencv2",
+            url: "https://github.com/oursky/formx-sdk/releases/download/0.1.0/FormX.xcframework.zip",
+            checksum: ""
+        )
+    ]
+)
