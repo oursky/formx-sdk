274cb91ff7059b3083eb062a9b563d3086aa5e79
FormX <hello@formx.ai>
2023-06-27 10:06:45 +0000

    release: v0.1.16

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FormX",
+    platforms: [.iOS(.v11), .macOS(.v10_14)],
+    products: [
+        .library(
+            name: "FormX",
+            targets: ["FormXTarget"])
+    ],
+    targets: [
+        .target(name: "FormXTarget",
+            dependencies: ["FormX"],
+            path: ".",
+            linkerSettings: [
+                .linkedLibrary("c++"),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedFramework("Accelerate")
+            ]
+        ),
+        .binaryTarget(name: "FormX",
+            url: "https://github.com/oursky/formx-sdk/releases/download/0.1.16/FormX.xcframework.zip",
+            checksum: "7b5ee951cf3f3a4a868abfe09a3b76878438d638813f6c4cbd30161d012faed2"
+        )
+    ]
+)
