398e2535aab669590da201d93ff46639e042cd22
FormX <hello@formx.ai>
2023-08-16 11:39:28 +0000

    release: v0.1.29

---
+++ FormX.podspec
@@ -0,0 +1,23 @@
+Pod::Spec.new do |s|
+    s.name             = 'FormX'
+    s.version          = '0.1.29'
+    s.summary          = 'FormX SDK for iOS'
+    s.homepage         = 'https://github.com/oursky/formx-sdk'
+    s.author           = { 'FormX' => 'hello@formx.ai' }
+    s.swift_version = ['5.0', '5.1', '5.2']
+
+    s.source       = { :http => "https://github.com/oursky/formx-sdk/releases/download/0.1.29/FormX.xcframework.zip" }
+    s.vendored_frameworks = 'FormX.xcframework'
+  
+    s.ios.deployment_target = '11.0'
+    s.ios.frameworks = ["Accelerate"]
+    s.osx.deployment_target = "10.15"
+    s.osx.frameworks = ['OpenCL', 'Accelerate']
+    s.libraries = 'c++'
+
+    s.prepare_command = <<-CMD
+        curl -O -L "#{s.source[:http]}"
+        unzip -o FormX.xcframework.zip
+        rm FormX.xcframework.zip
+    CMD
+end
---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FormX",
+    platforms: [.iOS(.v11), .macOS(.v10_14)],
+    products: [
+        .library(
+            name: "FormX",
+            targets: ["FormXTarget"])
+    ],
+    targets: [
+        .target(name: "FormXTarget",
+            dependencies: ["FormX"],
+            path: ".",
+            linkerSettings: [
+                .linkedLibrary("c++"),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedFramework("Accelerate")
+            ]
+        ),
+        .binaryTarget(name: "FormX",
+            url: "https://github.com/oursky/formx-sdk/releases/download/0.1.29/FormX.xcframework.zip",
+            checksum: "e83ad3c61d1fdfe715653d6919f340fe01ce23acf9c5601a53796134957671d7"
+        )
+    ]
+)
